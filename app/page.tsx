6f22267cb90582f11a43978012f4a4b6ff4f0ea5
SANDIP GHOSH <sandipghosh591@gmail.com>
2025-03-20 12:01:51 +0530

    update

--- app/page.tsx
+++ app/page.tsx
@@ -3,17 +3,17 @@ import HomePage from "./layout/components/home/page";
 import ProjectsPage from "./layout/components/project/page";
 import Footer from "./layout/footer/page";
 import Header from "./layout/header/page";
-import styles from "./page.module.css";
-import Link from "next/link";
-
 export default function Home() {
   return (
-    <div >
-      <Header/> 
-       <HomePage/>
-       <AboutPage/>
-       <ProjectsPage/>
-      <Footer/>
-    </div>
+    <>
+      
+          <Header />
+          <HomePage />
+          <AboutPage />
+          <ProjectsPage />
+          <Footer />
+        
+    </>
+
   );
 }
